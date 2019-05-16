1c4460c4b9c923e70ecd0f5c4f8a26019eea425e
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-05-16 16:43:14 -0500

    updated

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,70 @@
+
 class Artist
 
   attr_reader :name, :years_experience
 
+  @@all=[]
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+      artist_paintings= Painting.all.select do |paintings|
+        paintings.artist == self
+      end
+      artist_paintings.map do |painting|
+        painting
+      end
+    end
+      
+  def galleries
+      artist_paintings= Painting.all.select do |paintings|
+        paintings.artist == self
+      end
+      artist_paintings.map do |painting|
+        painting.gallery.name
+      end
+    end
+
+    def cities
+      artist_paintings= Painting.all.select do |paintings|
+        paintings.artist == self
+      end
+      unique_city = artist_paintings.map do |painting|
+        painting.gallery.city
+      end
+      unique_city.uniq
+    end
+
+    def self.total_experience
+      Artist.all.sum(&:years_experience)
+    end
+
+    def count_paintings
+      artist_paintings= Painting.all.select do |paintings|
+        paintings.artist == self
+      end
+        number_of_paintings = artist_paintings.map do |painting|
+          painting.title
+        end
+        number_of_paintings.length 
+      end
+
+    def most_prolific
+        
+    end
+
+    def create_painting(title, price, gallery)
+      Painting.new(title, price, self, gallery)
+    end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,20 @@
 require_relative '../config/environment.rb'
 
+me = Artist.new("Feven", 5)
+stefan = Artist.new("Stefan", 1)
+raul = Artist.new("Raul", 4)
+
+houston = Gallery.new("Flatiron", "Houston")
+austin = Gallery.new("UT", "Austin")
+
+new_painting = Painting.new("Trees", 10, me, houston)
+new_painting2 = Painting.new("Suns", 2, me, houston)
+new_painting3 = Painting.new("Buildings", 5, stefan, austin)
+new_painting4 = Painting.new("Pillars", 20, stefan, austin)
+
+
+
+
 
 
 
