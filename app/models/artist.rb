69263f08a73fe1b0921b39d738e361fa79e85bed
Roysan Easo <easo.roysan@gmail.com>
2019-05-16 14:25:52 -0500

    First draft

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,51 @@
 class Artist
 
-  attr_reader :name, :years_experience
+  attr_reader :name, :years_experience, :all
+
+    @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select do | painting |
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    Gallery.all.select do | gallery |
+      gallery.artist == self
+    end
+  end
+
+  def cities
+    self.galleries.map do |gallery|
+      gallery.city
+    end.uniq
+  end
+
+  def self.total_experience
+    self.all.inject(0) { |sum,artist| sum+artist.years_experience }
+  end
+
+  def self.most_prolific
+    most_prolific_artist = self.all[0]
+    best_paintings_per_year = (most_prolific_artist.paintings.length) / (most_prolific_artist.years_experience)
+    Artist.all.each do | artist |
+      paintings_per_year = (artist.paintings.length) / (artist.years_experience)
+      if best_paintings_per_year < paintings_per_year
+        most_prolific_artist = artist
+      end
+    end
+    most_prolific_artist
+  end
+
+  def create_painting(title,price,gallery)
+    title = Painting.new(title,price,self,gallery)
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,15 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+Leo = Artist.new('Da Vinci', 40)
+Van_Gogh = Artist.new('Vincent', 40)
+Matisse = Artist.new('Henri', 40)
+Louvre = Gallery.new('The Loo', 'Paris')
+MFA = Gallery.new('Dirty South Gallery', 'Houston')
+painting_one = Painting.new('Mona Lisa', 1000000, Leo, Louvre)
+painting_two = Painting.new('The Last Supper', 900000, Leo, Louvre)
+painting_three = Painting.new('Starry Night', 700000, Van_Gogh, MFA)
+painting_four = Painting.new('Woman with a Hat', 650000, Matisse, MFA)
+
+binding.pry
